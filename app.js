2cb891f8f0f3faa1262f3c5b1305620cacf417ba
CloverMidori <youhuid07@gmail.com>
2020-11-05 22:08:41 +0900

    あ

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 import { MDCRipple } from '@material/ripple/index';
+import {MDCTopAppBar} from '@material/top-app-bar';
 
 const ripple = new MDCRipple(document.querySelector('.mdc-button'));
+const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+const topAppBar = new MDCTopAppBar(topAppBarElement);
