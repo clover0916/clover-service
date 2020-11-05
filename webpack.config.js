62db3b1c24c9148ce9fb0d732ed04b9038bb8a50
CloverMidori <youhuid07@gmail.com>
2020-11-05 19:42:05 +0900

    あ

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+module.exports = [{
+  entry: './app.scss',
+  output: {
+    // This is necessary for webpack to compile
+    // But we never use style-bundle.js
+    filename: 'style-bundle.js',
+  },
+  devServer: {
+    host: '0.0.0.0',
+    port: 80,
+    disableHostCheck: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'bundle.css',
+            },
+          },
+          { loader: 'extract-loader' },
+          { loader: 'css-loader' },
+          {
+            loader: 'sass-loader',
+            options: {
+              // Prefer Dart Sass
+              implementation: require('sass'),
+
+              // See https://github.com/webpack-contrib/sass-loader/issues/804
+              webpackImporter: false,
+            },
+          },
+        ]
+      }
+    ]
+  },
+}];
