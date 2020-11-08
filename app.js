21e02acdae266cd85c6404b05650b35d5ab84af3
CloverMidori <youhuid07@gmail.com>
2020-11-08 22:18:32 +0900

    あ

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-import { MDCRipple } from '@material/ripple';
-
-const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
-
 import {MDCTopAppBar} from '@material/top-app-bar';
 
 // Instantiation
@@ -15,4 +11,11 @@ const topAppBars = MDCTopAppBar.attachTo(document.getElementById('app-bar'));
 topAppBars.setScrollTarget(document.getElementById('main-content'));
 topAppBars.listen('MDCTopAppBar:nav', () => {
   drawer.open = !drawer.open;
+});
+
+import {MDCRipple} from '@material/ripple';
+
+const selector = '.mdc-button, .mdc-icon-button, .mdc-card__primary-action';
+const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
+  return new MDCRipple(el);
 });
