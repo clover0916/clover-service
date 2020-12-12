a2d5b0379ecfe1a405ea0c97917f9cd1f081be3b
CloverMidori <youhuid07@gmail.com>
2020-12-12 18:56:13 +0900

    あ

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import {MDCRipple} from '@material/ripple';
 import {MDCTopAppBar} from '@material/top-app-bar';
+import {MDCDrawer} from "@material/drawer";
 
-const ripple = new MDCRipple(document.querySelector('.foo-button'));
+const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
 
 // インスタンス化
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
@@ -9,3 +10,14 @@ const topAppBar = new MDCTopAppBar(topAppBarElement);
 
 const iconButtonRipple = new MDCRipple(document.querySelector('.mdc-icon-button'));
 iconButtonRipple.unbounded = true;
+
+const listEl = document.querySelector('.mdc-drawer .mdc-list');
+const mainContentEl = document.querySelector('.main-content');
+
+listEl.addEventListener('click', (event) => {
+  drawer.open = false;
+});
+
+document.body.addEventListener('MDCDrawer:closed', () => {
+  mainContentEl.querySelector('input, button').focus();
+});
