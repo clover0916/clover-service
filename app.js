b8738d8e94cd0ea1a7ee310f405ee319322bb188
CloverMidori <youhuid07@gmail.com>
2020-12-12 18:50:06 +0900

    あ

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-import {MDCRipple} from '@material/ripple/index';
+import {MDCRipple} from '@material/ripple';
+import {MDCTopAppBar} from '@material/top-app-bar';
+
 const ripple = new MDCRipple(document.querySelector('.foo-button'));
+
+// インスタンス化
+const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+const topAppBar = new MDCTopAppBar(topAppBarElement);
+
+const iconButtonRipple = new MDCRipple(document.querySelector('.mdc-icon-button'));
+iconButtonRipple.unbounded = true;
