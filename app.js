3b0ff24199df362e18705bd7942a4e07dbcbc3dc
CloverMidori <youhuid07@gmail.com>
2020-12-12 14:45:47 +0900

    あ

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import {MDCTopAppBar} from '@material/top-app-bar';
 
 // Instantiation
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
-const topAppBar = new MDCTopAppBar(topAppBarElement);
 
 const iconButtonRipple = new MDCRipple(document.querySelector('.mdc-icon-button'));
 iconButtonRipple.unbounded = true;
