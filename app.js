34dcf67c179e24ba46dd4e780b3fb2bf2964edda
CloverMidori <youhuid07@gmail.com>
2020-12-12 14:17:21 +0900

    あ

--- app.js
+++ app.js
@@ -7,9 +7,25 @@ const topAppBar = new MDCTopAppBar(topAppBarElement);
 const iconButtonRipple = new MDCRipple(document.querySelector('.mdc-icon-button'));
 iconButtonRipple.unbounded = true;
 
-const topAppBars = MDCTopAppBar.attachTo(document.getElementById('app-bar'));
-topAppBars.setScrollTarget(document.getElementById('main-content'));
-topAppBars.listen('MDCTopAppBar:nav', () => {
+import {MDCList} from "@material/list";
+const list = MDCList.attachTo(document.querySelector('.mdc-list'));
+list.wrapFocus = true;
+
+const listEl = document.querySelector('.mdc-drawer .mdc-list');
+const mainContentEl = document.querySelector('.main-content');
+
+listEl.addEventListener('click', (event) => {
+  mainContentEl.querySelector('input, button').focus();
+});
+
+document.body.addEventListener('MDCDrawer:closed', () => {
+  mainContentEl.querySelector('input, button').focus();
+});
+
+import {MDCTopAppBar} from "@material/top-app-bar";
+const topAppBar = MDCTopAppBar.attachTo(document.getElementById('app-bar'));
+topAppBar.setScrollTarget(document.getElementById('main-content'));
+topAppBar.listen('MDCTopAppBar:nav', () => {
   drawer.open = !drawer.open;
 });
 
