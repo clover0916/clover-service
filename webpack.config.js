74d15b797c552ee5c3fd01e00919b7445ced14ce
CloverMidori <youhuid07@gmail.com>
2020-12-13 18:36:47 +0900

    あ

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,21 @@
 const autoprefixer = require('autoprefixer');
-var nodeExternals = require('webpack-node-externals');
+const nodeExternals = require('webpack-node-externals');
+const path = require('path');
 
 module.exports = [{
-  target: 'node',
-  externals: [nodeExternals()],
   entry: ['./app.js', './app.scss'],
   output: {
-    filename: 'bundle.js',
-    path: __dirname + 'public/dist'
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].js'
   },
+  target: 'node',
+    node: {
+      // Need this when working with express, otherwise the build fails
+      __dirname: false, // if you don't put this is, __dirname
+      __filename: false, // and __filename return blank or /
+    },
+  externals: [nodeExternals()],
   mode: "development",
   devServer: {
     port: process.env.PORT || 5000,
