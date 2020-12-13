0bf94c767622d9c2317a79dab88e51144b5f2e19
CloverMidori <youhuid07@gmail.com>
2020-12-13 13:35:33 +0900

    あ

--- app.js
+++ app.js
@@ -29,3 +29,18 @@ listEl.addEventListener('click', (event) => {
 document.body.addEventListener('MDCDrawer:closed', () => {
   mainContentEl.querySelector('input, button').focus();
 });
+
+window.WebFontConfig = {
+        google: { families: ['Roboto', 'Noto+Sans+JP', 'Material+Icons'] },
+        active: function() {
+        sessionStorage.fonts = true;
+      }
+      };
+      (function() {
+        var wf = document.createElement('script');
+        wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+        wf.type = 'text/javascript';
+        wf.async = 'true';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(wf, s);
+      })();
