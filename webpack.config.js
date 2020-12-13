cf8ee4e7ee568f1ade14e0e044b1579ef4b350dd
CloverMidori <youhuid07@gmail.com>
2020-12-13 19:16:38 +0900

    あ

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = [{
   output: {
     path: path.join(__dirname, 'public', 'dist'),
     publicPath: '/',
-    filename: '[name].js'
+    filename: 'bundle.js'
   },
   target: 'node',
     node: {
