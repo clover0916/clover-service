077cca86b1d74cea314a288ad9225bd8d2997301
CloverMidori <youhuid07@gmail.com>
2020-12-13 19:36:19 +0900

    あ

--- webpack.config.js
+++ webpack.config.js
@@ -51,10 +51,10 @@ module.exports = [{
       },
       {
         test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env']
+        },
       }
     ]
   },
