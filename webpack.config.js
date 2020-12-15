7102f3c770e4e995f3a4b6905ccb3391a2eb93b1
CloverMidori <youhuid07@gmail.com>
2020-12-15 22:07:22 +0900

    あ

--- webpack.config.js
+++ webpack.config.js
@@ -67,6 +67,7 @@ module.exports = [{
               sassOptions: {
                           includePaths: ['./node_modules']
               },
+              sourceMap: true,
             },
           },
         ]
