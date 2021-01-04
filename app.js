59132213ff3052936b06a6efbb4e318b21302e9e
Clover_Midori <64039436+CloverMidori@users.noreply.github.com>
2021-01-05 00:58:04 +0900

    update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(function (req, res, next) {
-  res.header('Cache-Control', 'no-store');
+  res.header('Cache-Control', 'no-store', 'max-age=0');
   next();
 });
 
