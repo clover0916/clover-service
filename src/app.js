9941a2abbbcb5b7f9cfa82d6d1db44afbb108031
CloverMidori <youhuid05@gmail.com>
2021-01-17 05:49:40 +0900

    あ

--- src/app.js
+++ src/app.js
@@ -46,3 +46,16 @@ var xhr = new XMLHttpRequest(),
 		}
 	};
 	xhr.send();
+	
+document.getElementById("OpenDialog").onclick = function() {
+  var url = document.getElementById("OpenDialog").dataset.url;
+  dialog.open();
+  document.getElementById("url").innerHTML = url;
+  const go = document.getElementById("go-url");
+  go.addEventListener('click', function() {
+    const spinner = document.getElementById("load");
+    spinner.classList.remove("hidden");
+    go.classList.add("hidden");
+    location.href = url;
+  });
+};
