fdcf457bd8f687a60bdf15a2e444a790b30731ac
CloverMidori <youhuid05@gmail.com>
2021-01-17 05:35:40 +0900

    あ

--- src/app.js
+++ src/app.js
@@ -3,59 +3,6 @@ import { MDCTopAppBar } from '@material/top-app-bar';
 import { MDCCircularProgress } from '@material/circular-progress';
 import { MDCDialog } from '@material/dialog';
 
-// BootstrapのJavaScript側の機能を読み込む
-//import "bootstrap";
-
-
-// インスタンス化
-const topAppBarElement = document.querySelector('.mdc-top-app-bar');
-const topAppBarInstance = new MDCTopAppBar(topAppBarElement);
-const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
-const circularProgress = new MDCCircularProgress(document.querySelector('.mdc-circular-progress'));
-const dialog = new MDCDialog(document.querySelector('.mdc-dialog'));
-const iconButtonRipple = new MDCRipple(document.querySelector('.mdc-icon-button'));
-iconButtonRipple.unbounded = true;
-
-const selector = '.mdc-button, .mdc-icon-button, .mdc-card__primary-action';
-const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
-  return new MDCRipple(el);
-});
-
-window.addEventListener('load', ae);
-
-function ae(){
-  alert('load')
-}
-
-document.getElementById("OpenDialog").onclick = function() {
-  var url = document.getElementById("OpenDialog").dataset.url;
-  dialog.open();
-  document.getElementById("url").innerHTML = url;
-  const go = document.getElementById("go-url");
-  go.addEventListener('click', function() {
-    const spinner = document.getElementById("load");
-    spinner.classList.remove("hidden");
-    go.classList.add("hidden");
-    location.href = url;
-  });
-};
-
-/*var xhr = new XMLHttpRequest(),
-		method = "GET",
-		url = "../dialog.html";//読み込まれるHTMLを指定
-	var box=document.getElementById("dialog");//読み込みたい位置を指定
- 
-	xhr.open(method, url, true);
-	xhr.onreadystatechange = function () {
-		if(xhr.readyState === 4 && xhr.status === 200) {
-		  alert('ho?')
-			var restxt=xhr.responseText;//String型で取得
-			dialog.innerHTML=restxt;//完了
-		}
-	};
-	xhr.send();
-*/
-
 window.WebFontConfig = {
   google: { families: ['Roboto', 'Noto+Sans+JP', 'Material+Icons'] },
   typekit: { id: 'pbk7rhd' },
@@ -71,3 +18,45 @@ window.WebFontConfig = {
   var s = document.getElementsByTagName('script')[0];
   s.parentNode.insertBefore(wf, s);
 })();
+
+// インスタンス化
+const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+const topAppBarInstance = new MDCTopAppBar(topAppBarElement);
+const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
+const circularProgress = new MDCCircularProgress(document.querySelector('.mdc-circular-progress'));
+const dialog = new MDCDialog(document.querySelector('.mdc-dialog'));
+const iconButtonRipple = new MDCRipple(document.querySelector('.mdc-icon-button'));
+iconButtonRipple.unbounded = true;
+
+const selector = '.mdc-button, .mdc-icon-button, .mdc-card__primary-action';
+const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
+  return new MDCRipple(el);
+});
+
+const loader = document.getElementById('loading');
+window.addEventListener('load', () => {
+  const ms = 400;
+  loader.style.transition = 'opacity ' + ms + 'ms';
+  
+  const loaderOpacity = function() {
+    loader.style.opacity = 0;
+  }
+  const loaderDisplay = function() {
+    loader.style.display = 'none';
+  }
+  setTimeout(loaderOpacity, 1);
+  setTimeout(loaderDisplay, ms); // opacityが0になるまで待ってからdisplay: none;にする
+});
+
+document.getElementById("OpenDialog").onclick = function() {
+  var url = document.getElementById("OpenDialog").dataset.url;
+  dialog.open();
+  document.getElementById("url").innerHTML = url;
+  const go = document.getElementById("go-url");
+  go.addEventListener('click', function() {
+    const spinner = document.getElementById("load");
+    spinner.classList.remove("hidden");
+    go.classList.add("hidden");
+    location.href = url;
+  });
+};
