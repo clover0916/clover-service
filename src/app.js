7a38b7db201a3569d67f83042e38a1d223ed2b97
CloverMidori <youhuid05@gmail.com>
2021-01-17 06:00:19 +0900

    あ

--- src/app.js
+++ src/app.js
@@ -33,20 +33,6 @@ const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
   return new MDCRipple(el);
 });
 
-var xhr = new XMLHttpRequest(),
-		method = "GET",
-		url = "/dialog.html";//読み込まれるHTMLを指定
-	var box = document.getElementById("dialog");//読み込みたい位置を指定
- 
-	xhr.open(method, url, true);
-	xhr.onreadystatechange = function () {
-		if(xhr.readyState === 4 && xhr.status === 200) {
-			var restxt=xhr.responseText;//String型で取得
-			box.innerHTML=restxt;//完了
-		}
-	};
-	xhr.send();
-	
 document.getElementById("OpenDialog").onclick = function() {
   var url = document.getElementById("OpenDialog").dataset.url;
   dialog.open();
