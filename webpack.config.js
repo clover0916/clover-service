d7a20c46ebb5cc019ea8c0ac97c6b8d760fa1bf8
CloverMidori <youhuid05@gmail.com>
2021-01-16 13:38:39 +0900

    あ

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const autoprefixer = require('autoprefixer');
 const path = require('path');
 
 module.exports = {
@@ -25,18 +25,28 @@ module.exports = {
           { loader: 'extract-loader' },
           { loader: 'css-loader' },
           {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  autoprefixer()
+                ]
+              }
+            } 
+          },
+          {
             loader: 'sass-loader',
             options: {
-              // Prefer Dart Sass
-              implementation: require('sass'),
-          
-              // See https://github.com/webpack-contrib/sass-loader/issues/804
-              webpackImporter: false,
               sassOptions: {
                 includePaths: ['./node_modules']
               },
+              // Prefer Dart Sass
+              implementation: require('sass'),
+
+              // See https://github.com/webpack-contrib/sass-loader/issues/804
+              webpackImporter: false,
             }
-          }
+          },
         ]
       },
       {
