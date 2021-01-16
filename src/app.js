07ee9ff2e5d9dc9a4499ab39ec5b3d6a14ae636b
CloverMidori <youhuid05@gmail.com>
2021-01-16 14:12:52 +0900

    あ

--- src/app.js
+++ src/app.js
@@ -25,10 +25,15 @@ const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
   return new MDCRipple(el);
 });
 
-$(window).on('load',function(){
-  alert('load')
-  $("loading").fadeOut();
+$(window).load(function() {
+  // 消えるタイミングはお好みで
+  $('#loading').delay(1500).fadeOut(300);
 });
+// 10秒待っても読み込みが終わらない時は強制的にローディング画面をフェードアウト
+function stopload() {
+  $('#loading').delay(1000).fadeOut(700);
+}
+setTimeout('stopload()', 10000);
 
 document.getElementById("OpenDialog").onclick = function() {
   var url = document.getElementById("OpenDialog").dataset.url;
