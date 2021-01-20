4e5acb38b3a31a2daab47cf07ddc8b6e5aa9f25b
CloverMidori <youhuid05@gmail.com>
2021-01-20 22:03:37 +0900

    あ

--- src/youtube.js
+++ src/youtube.js
@@ -1,33 +1,6 @@
-const videoTag = document.getElementById("video");
-const myMediaSource = new MediaSource();
-const url = URL.createObjectURL(myMediaSource);
-videoTag.src = url;
+var video = document.getElementById('video');
+var movie_url = "http://127.0.0.1:3000/youtube?url=watch?v=QW28YKqdxe0";
 
-// 1. add source buffers
-
-const audioSourceBuffer = myMediaSource
-  .addSourceBuffer('audio/mp4; codecs="mp4a.40.2"');
-const videoSourceBuffer = myMediaSource
-  .addSourceBuffer('video/mp4; codecs="avc1.64001e"');
-
-// 2. download and add our audio/video to the SourceBuffers
-
-fetch("http://127.0.0.1:3000/youtube?url=watch?v=QW28YKqdxe0").then(function(response) {
-  // The data has to be a JavaScript ArrayBuffer
-  return response.arrayBuffer();
-}).then(function(audioData) {
-  audioSourceBuffer.appendBuffer(audioData);
-});
-
-// the same for the video SourceBuffer
-fetch("http://127.0.0.1:3000/youtube?url=watch?v=QW28YKqdxe0").then(function(response) {
-  // The data has to be a JavaScript ArrayBuffer
-  return response.arrayBuffer();
-}).then(function(videoData) {
-  videoSourceBuffer.appendBuffer(videoData);
-});
-
-video.load()
-
-video.play()
-
+video.src = movie_url;
+video.load();
+video.play();
