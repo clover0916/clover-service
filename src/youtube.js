b03c6fd9461195be2bb898930f22632d4b069fdc
CloverMidori <youhuid05@gmail.com>
2021-01-21 07:03:43 +0900

    あ

--- src/youtube.js
+++ src/youtube.js
@@ -13,18 +13,18 @@ const videoSourceBuffer = myMediaSource
 // 2. download and add our audio/video to the SourceBuffers
 
 // for the audio SourceBuffer
-fetch("https://clover-service.online/youtube?url=watch?v=QW28YKqdxe0", {method: 'GET'}).then((response) => {
-  alert(response)
+fetch("https://clover-service.online/youtube?url=watch?v=QW28YKqdxe0", {method: 'GET'}).then((res) => {
+  alert(res)
   // The data has to be a JavaScript ArrayBuffer
-  return response.arrayBuffer();
+  return res.arrayBuffer();
 }).then(function(audioData) {
   audioSourceBuffer.appendBuffer(audioData);
 });
 
 // the same for the video SourceBuffer
-fetch("https://clover-service.online/youtube?url=watch?v=QW28YKqdxe0", {method: 'GET'}).then((response) => {
+fetch("https://clover-service.online/youtube?url=watch?v=QW28YKqdxe0", {method: 'GET'}).then((res) => {
   // The data has to be a JavaScript ArrayBuffer
-  return response.arrayBuffer();
+  return res.arrayBuffer();
 }).then(function(videoData) {
   videoSourceBuffer.appendBuffer(videoData);
 });
