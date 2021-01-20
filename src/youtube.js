8f70dc945f8fbc40238be7d3694b068777b3a995
CloverMidori <youhuid05@gmail.com>
2021-01-20 22:07:29 +0900

    あ

--- src/youtube.js
+++ src/youtube.js
@@ -1,5 +1,5 @@
 var video = document.getElementById('video');
-var movie_url = "http://127.0.0.1:3000/youtube?url=watch?v=QW28YKqdxe0";
+var movie_url = "https://clover-service.online/youtube?url=watch?v=QW28YKqdxe0";
 
 video.src = movie_url;
 video.load();
