d89e6c0363c013665c25fbb02d53dee0ed936549
CloverMidori <youhuid05@gmail.com>
2021-01-20 18:30:30 +0900

    あ

--- src/youtube.js
+++ src/youtube.js
@@ -2,7 +2,7 @@ const ytdl = require('ytdl-core');
 
 var video = document.getElementById('video');
 
-video.src = ytdl('https://youtu.be/QW28YKqdxe0' ,{ filter: format => format.container === 'mp4' });
+video.src = ytdl('https://youtu.be/QW28YKqdxe0').pipe(fs.createWriteStream('video.mp4'));
 
 video.load()
 
