5dc8a4f4c826874f0e5c11e1798b8418483544f2
CloverMidori <youhuid05@gmail.com>
2021-01-20 21:01:15 +0900

    あ

--- routes/youtube.js
+++ routes/youtube.js
@@ -5,21 +5,13 @@ const ytdl = require('ytdl-core');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  var videoUrl = req.query.videoUrl;
-  
-  var videoReadableStream = ytdl(videoUrl);
-  
-  ytdl.getInfo(videoUrl, function(err, info) {
-    var videoName = info.title.replace('|', '').toString('ascii');
-  
-    var videoWritableStream = fs.createWriteStream('..' + '/public' + videoName + '.mp4');
-  
-    var stream = videoReadableStream.pipe(videoWritableStream);
-  
-    stream.on('finish', function() {
-      res.writeHead(204);
-      res.end();
-    });
+  var URL = req.query.url;
+  var stream = ytdl(URL);
+  stream.on('info', (info) => {
+    res.header('Content-Disposition', 'attachment; filename="video.mp4"');
+    ytdl(URL, {
+      format: 'mp4'
+    }).pipe(res);
   });
 });
 
