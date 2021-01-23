30432919f1527631877b8345b3f786f7d10a8ea0
CloverMidori <youhuid05@gmail.com>
2021-01-23 10:45:48 +0900

    あ

--- routes/youtube.js
+++ routes/youtube.js
@@ -15,7 +15,6 @@ router.get('/', async function(req, res, next) {
   var stream = ytdl(URL, { format: 'mp4'});
 
   stream.on('info', (info) => {
-    console.log(info)
     var title = encodeURIComponent(info.player_response.videoDetails.title + '.mp4')
     res.header('Content-Disposition', 'attachment; filename*=UTF-8\'\'' + title);
     ytdl(URL, { format: 'mp4'})
