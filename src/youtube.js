62377bfcbc7935fc58970c315c429671c6ead57e
CloverMidori <youhuid05@gmail.com>
2021-01-23 14:08:57 +0900

    あ

--- src/youtube.js
+++ src/youtube.js
@@ -1,21 +1,5 @@
 const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
 
-textField.listen('input', function(){
-  textField.helperText_.foundation.setPersistent(true);
-
-  if(!textField.value) {
-    textField.valid = false;
-    textField.helperTextContent = 'URLを入力してください';
-    return;
-  }
-
-  textField.valid = true;
-
-  textField.helperText_.foundation.setPersistent(false);
-  // or
-  //textField.helperTextContent = 'Valid';
-});
-
 document.getElementById('get_video').addEventListener('click', function() {
   var video_url = document.getElementById("video_url").value
   video_url = document.getElementById("video_url").value.replace('https://www.youtube.com/', '')
