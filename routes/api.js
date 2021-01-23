0d18c20074ec6ae7d4116ef59b0250f7a28360b0
CloverMidori <youhuid05@gmail.com>
2021-01-23 14:53:05 +0900

    あ

--- routes/api.js
+++ routes/api.js
@@ -9,10 +9,18 @@ router.get('/get_video', async function(req, res, next) {
   var URL = 'https://www.youtube.com/' + url 
   
   const video = ytdl(url,{filter: (format) => format.container === 'mp4' });
-  var title = encodeURIComponent(video.player_response.videoDetails.title + '.mp4')
-  
-  res.header('Content-Disposition', 'attachment; filename*=UTF-8\'\'' + title);
+
   video.pipe(res);
+  
+  video.on('end', () => {
+    //DLしたYoutube動画の情報
+    ytdl.getInfo(URL, (err, info) => {
+      if (err) throw err;
+      var title = encodeURIComponent(video.player_response.videoDetails.title + '.mp4')
+      
+      res.header('Content-Disposition', 'attachment; filename*=UTF-8\'\'' + title);
+    });
+  });
 });
 
 module.exports = router;
