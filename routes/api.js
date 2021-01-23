0f0fc96a5433fe23d5cf9bacb821fc24f62b3582
CloverMidori <youhuid05@gmail.com>
2021-01-23 12:04:03 +0900

    Fix Bug

--- routes/api.js
+++ routes/api.js
@@ -9,8 +9,8 @@ router.get('/get_video', async function(req, res, next) {
   var URL = 'https://www.youtube.com/' + url 
   
   const video = ytdl(url,{filter: (format) => format.container === 'mp4' });
-
-  var title = encodeURIComponent(info.player_response.videoDetails.title + '.mp4')
+  var title = encodeURIComponent(video.player_response.videoDetails.title + '.mp4')
+  
   res.header('Content-Disposition', 'attachment; filename*=UTF-8\'\'' + title);
   video.pipe(res);
 });
