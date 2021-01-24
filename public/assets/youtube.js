a7f017fb08dbae4cad85b2f6e0ab50ce1d176f08
CloverMidori <youhuid05@gmail.com>
2021-01-24 17:58:37 +0900

    あ

--- public/assets/youtube.js
+++ public/assets/youtube.js
@@ -3,30 +3,37 @@ const myMediaSource = new MediaSource();
 const url = URL.createObjectURL(myMediaSource);
 videoTag.src = url;
 
-const file = 'QW28YKqdxe0'
+document.getElementById('go').addEventListener('click', function() {
+  document.getElementById("url").value = document.getElementById("url").value.replace('https://www.youtube.com/watch?v=', '')
+  if (!document.getElementById("video_url").value) {
+    return;
+  }
+  //submit()でフォームの内容を送信
+  get_video(document.getElementById("url").value)
+})
 
 // 1. add source buffers
 
 // for the audio SourceBuffer
-fetch('https://api.clover-service.online/video_info?id=' + file, { method: 'GET' })
-  .then(response => response.json())
-  .then(data => {
-    const video = JSON.parse(data)
-    
-    const videoSourceBuffer = myMediaSource.addSourceBuffer(`${video.formats[0].mimeType}; codecs="${video.formats[0].codecs}"`);
-    
-    // 2. download and add our audio/video to the SourceBuffers
-    
-    // the same for the video SourceBuffer
-    fetch("https://api.clover-service.online/get_video?id=" + file, { method: 'GET' })
-      .then(response => response.arrayBuffer())
-      .then(data => {
-        videoSourceBuffer.appendBuffer(data);
-      });
-  })
-  .catch(err => {
-    console.error(err);
-  });
-  //.then(function(audioData) {
-  //audioSourceBuffer.appendBuffer(audioData);
-//});
+function get_video(file) {
+  fetch('https://api.clover-service.online/video_info?id=' + file, { method: 'GET' })
+    .then(response => response.json())
+    .then(data => {
+      const video = JSON.parse(data)
+  
+  
+      const videoSourceBuffer = myMediaSource.addSourceBuffer(`${video.formats[0].mimeType}; codecs="${video.formats[0].codecs}"`);
+  
+      // 2. download and add our audio/video to the SourceBuffers
+  
+      // the same for the video SourceBuffer
+      fetch("https://api.clover-service.online/get_video?id=" + file, { method: 'GET' })
+        .then(response => response.arrayBuffer())
+        .then(data => {
+          videoSourceBuffer.appendBuffer(data);
+        });
+    })
+    .catch(err => {
+      console.error(err);
+    });
+}
