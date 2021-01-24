c43746bed9d0df00d27e2ee1e76371befe0dafd5
Clover Midori <youhuid05@gmail.com>
2021-01-24 00:55:35 +0000

    a

--- routes/api.js
+++ routes/api.js
@@ -9,8 +9,6 @@ router.get('/get_video', async function(req, res, next) {
   var URL = 'https://www.youtube.com/' + url 
   
   const video = ytdl(url,{filter: (format) => format.container === 'mp4' });
-
-  video.pipe(res);
   
   video.on('end', () => {
     //DLしたYoutube動画の情報
@@ -19,6 +17,7 @@ router.get('/get_video', async function(req, res, next) {
       var title = encodeURIComponent(video.player_response.videoDetails.title + '.mp4')
       
       res.header('Content-Disposition', 'attachment; filename*=UTF-8\'\'' + title);
+      video.pipe(res);
     });
   });
 });
