d2a458e8371c973ebb330f886c4f963d1f05f1a8
CloverMidori <youhuid05@gmail.com>
2021-02-14 13:05:09 +0900

    あ

--- src/commands.js
+++ src/commands.js
@@ -2,16 +2,17 @@
 import {MDCDataTable} from '@material/data-table';
 import { MDCDialog } from '@material/dialog';
 
+
 const dataTable = new MDCDataTable(document.querySelector('.mdc-data-table'));
 const dialog = new MDCDialog(document.querySelector('.mdc-dialog'));
 
 
-document.getElementById('od').onclick = function() {
-    fetch("../assets/commands.json")
-      .then(res => res.json())
-      .then(data => {
-        const json = data.find((v) => v.name === document.getElementById("od").dataset.command)
-        dialog.open()
-        document.getElementById("detail").innerHTML = json.description;
-      })
-  }
+function od() {
+  fetch("../assets/commands.json")
+    .then(res => res.json())
+    .then(data => {
+      const json = data.find((v) => v.name === document.getElementById("od").dataset.command)
+      dialog.open()
+      document.getElementById("detail").innerHTML = json.description;
+    })
+}
