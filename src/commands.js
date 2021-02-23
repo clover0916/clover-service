a5926d023dc4b4574796e1b66adc6342efe66cab
CloverMidori <youhuid05@gmail.com>
2021-02-23 21:51:45 +0900

    あ

--- src/commands.js
+++ src/commands.js
@@ -8,12 +8,14 @@ const dialog = new MDCDialog(document.querySelector('.mdc-dialog'));
 var OpenDialog = document.querySelectorAll('.od');
 
 for (var i = 0; i < OpenDialog.length; i++) {
-  var command = OpenDialog[i].dataset.command
+  var b = {
+    [i]: OpenDialog[i].dataset.command
+  };
   OpenDialog[i].addEventListener('click', function() {
     fetch("../assets/commands.json")
       .then(res => res.json())
       .then(data => {
-        const json = data.find((v) => v.name === command);
+        const json = data.find((v) => v.name === b[i]);
         dialog.open();
         document.getElementById("detail").innerHTML = json.description;
     })
