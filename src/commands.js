c744299e1257370c90f319e838d8959a10ceb6cc
CloverMidori <youhuid05@gmail.com>
2021-02-24 21:31:25 +0900

    あ

--- src/commands.js
+++ src/commands.js
@@ -8,16 +8,13 @@ const dialog = new MDCDialog(document.querySelector('.mdc-dialog'));
 var OpenDialog = document.querySelectorAll('.od');
 
 for (var i = 0; i < OpenDialog.length; i++) {
-  var b = {
-    [i]: OpenDialog[i].dataset.command
-  };
-  OpenDialog[i].addEventListener('click', function() {
+  OpenDialog[i].onclick = function() {
     fetch("../assets/commands.json")
       .then(res => res.json())
       .then(data => {
-        const json = data.find((v) => v.name === b[i]);
+        const json = data.find((v) => v.name === OpenDialog[i].dataset.command);
         dialog.open();
         document.getElementById("detail").innerHTML = json.description;
     })
-  }, false);
+  };
 }
