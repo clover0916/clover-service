08985188ff2f4b1f3fd40a61a89511c32cba16f3
CloverMidori <youhuid05@gmail.com>
2021-03-05 22:26:05 +0900

    あ

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import { MDCDialog } from '@material/dialog';
 import { MDCCircularProgress } from '@material/circular-progress';
+import {MDCMenu} from '@material/menu';
 
 const circularProgress = new MDCCircularProgress(document.querySelector('.mdc-circular-progress'));
 const dialog = new MDCDialog(document.querySelector('.mdc-dialog'));
-
+const menu = new MDCMenu(document.querySelector('.mdc-menu'));
 
 document.getElementById("OpenDialog").onclick = function() {
   var url = document.getElementById("OpenDialog").dataset.url;
@@ -17,3 +18,9 @@ document.getElementById("OpenDialog").onclick = function() {
     location.href = url;
   });
 };
+
+document.getElementById('om').onclick = function () {
+  menu.open = !menu.open
+  menu.setFixedPosition(true);
+  menu.setAnchorElement(document.getElementById('om'));
+}
