9ad2cd4a4c6448b9d0e9c506db16ce353ffbb492
CloverMidori <youhuid05@gmail.com>
2021-03-25 23:01:28 +0900

    あ

--- src/index.js
+++ src/index.js
@@ -17,15 +17,16 @@ function od(title, description, redirect) {
   dialog.open();
   document.getElementById('my-dialog-title').innerHTML = title;
   document.getElementById('my-dialog-content').innerHTML = description;
-  if(redirect) {
+  if (redirect) {
     const go = document.getElementById("go-url");
     go.addEventListener('click', function() {
-    const spinner = document.getElementById("load");
-    spinner.classList.remove("hidden");
-    go.classList.add("hidden");
-    location.href = redirect;
-  });
-};
+      const spinner = document.getElementById("load");
+      spinner.classList.remove("hidden");
+      go.classList.add("hidden");
+      location.href = redirect;
+    });
+  };
+}
 
 document.getElementById('menu-button').onclick = function() {
   menu.open = !menu.open
@@ -34,7 +35,7 @@ document.getElementById('menu-button').onclick = function() {
 
 document.getElementById('ss').onclick = function() {
   var title = 'サポートサーバー';
-  var description  = `
+  var description = `
     <h2 style="mdc-typography--headline2">サポートサーバーとは？</h2>
     <div id="chat-container"><div id="chat-input"><div id="file-input"></div></div></div>
   `;
