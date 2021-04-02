a8e9bd79daea4b5bb890e6eda27e20116dd10332
CloverMidori <youhuid05@gmail.com>
2021-04-02 09:24:57 +0900

    あ

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ const topAppBarInstance = new MDCTopAppBar(topAppBarElement);
 const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
 const iconButtonRipple = new MDCRipple(document.querySelector('.mdc-icon-button'));
 const iconToggle = new MDCIconButtonToggle(document.querySelector('.mdc-icon-button'));
-const listRipple = new MDCRipple(document.querySelector('.mdc-list-item'));
+const listItemRipples = list.listElements.map((listItemEl) => new MDCRipple(listItemEl));
 const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
 const list = MDCList.attachTo(document.querySelector('.mdc-list'));
 const linearProgress = new MDCLinearProgress(document.querySelector('.mdc-linear-progress'));
