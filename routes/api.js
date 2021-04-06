337dc0339abc70cd6ba49c9a496acfa2dcdbb20c
Shimayu0916 <youhuid07@gmail.com>
2021-04-06 16:02:54 +0900

    Update

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,4 @@
+const { request } = require('express');
 var express = require('express');
 var router = express.Router();
 
@@ -5,17 +6,17 @@ const ytdl = require('ytdl-core');
 
 /* GET users listing. */
 router.get('/get_video', async function(req, res, next) {
-  var url = req.query.id;
-  var URL = 'https://www.youtube.com/watch?v=' + url 
-  
-  const video = ytdl(url,{filter: (format) => format.container === 'mp4' });
-  
-  const info = await ytdl.getInfo(URL);
-
-  var title = encodeURIComponent(info.videoDetails.title + '.mp4')
-      
-  res.header('Content-Disposition', 'attachment; filename*=UTF-8\'\'' + title);
-  video.pipe(res);
+  try {
+    var url = req.query.id;
+    var URL = 'https://www.youtube.com/watch?v=' + url 
+    const video = ytdl(url,{filter: (format) => format.container === 'mp4' });
+    const info = await ytdl.getInfo(URL);
+    var title = encodeURIComponent(info.videoDetails.title + '.mp4')    
+    res.header('Content-Disposition', 'attachment; filename*=UTF-8\'\'' + title);
+    video.pipe(res);
+  } catch (err) {
+    res.send("Too many request.")
+  }
 });
 
 router.get('/video_info', async function(req, res, next) {
