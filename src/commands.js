3e750028fcc7f7ae83387964be66a8bf47594735
Shimayu0916 <youhuid07@gmail.com>
2021-04-07 15:13:54 +0900

    Update

--- src/commands.js
+++ src/commands.js
@@ -33,8 +33,29 @@ fetch("../assets/commands.json")
         th.setAttribute('scope', "col");
         tr.appendChild(th);
       } else {
-        console.log("Naiyou");
-        console.log(element["name"]);
+        var tbody = commandTable.createTBody()
+        tbody.classList.add("mdc-data-table__content")
+        var tr = thead.insertRow(0);
+        tr.classList.add("mdc-data-table__header-row")
+        tr.setAttribute('data-row-id', `u${index - 1}`);
+        var th = document.createElement("th")
+        th.innerHTML = element["name"];
+        th.classList.add("mdc-data-table__cell")
+        th.setAttribute('scope', "row");
+        th.setAttribute('id', `u${index - 1}`);
+        tr.appendChild(th);
+        var td = document.createElement("td")
+        td.innerHTML = element["description"];
+        td.classList.add("mdc-data-table__cell")
+        tr.appendChild(td);
+        var td = document.createElement("td")
+        td.innerHTML = element["use"];
+        td.classList.add("mdc-data-table__cell")
+        tr.appendChild(td);
+        var td = document.createElement("td")
+        td.innerHTML = element["example"];
+        td.classList.add("mdc-data-table__cell")
+        tr.appendChild(td);
       }
     });
   })
