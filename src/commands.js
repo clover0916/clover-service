0dc7cfcf74e0c4fab1b87260abead697a5d95d04
Shimayu0916 <youhuid07@gmail.com>
2021-04-07 14:40:28 +0900

    Update

--- src/commands.js
+++ src/commands.js
@@ -1,14 +1,8 @@
 import { MDCDataTable } from '@material/data-table';
-import { MDCDialog } from '@material/dialog';
 
 
 const dataTable = new MDCDataTable(document.querySelector('.mdc-data-table'));
-const dialog = new MDCDialog(document.querySelector('.mdc-dialog'));
-
-var OpenDialog = document.querySelectorAll('.od');
 var commandTable = document.getElementById("cl")
-var command = {};
-var json;
 
 fetch("../assets/commands.json")
   .then(res => res.json())
@@ -48,16 +42,3 @@ fetch("../assets/commands.json")
       }
     });
   })
-
-/*for (var i = 0; i < OpenDialog.length; i++) {
-  command[i] = OpenDialog[i].dataset.command
-  OpenDialog[i].onclick = function() {
-    fetch("../assets/commands.json")
-      .then(res => res.json())
-      .then(data => {
-        json = data.find((v) => v === this.dataset.command);
-        dialog.open();
-        document.getElementById("detail").innerHTML = json.description;
-    })
-  };
-}*/
