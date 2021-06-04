e0a639e5c873227d5c2d8d18467ba3fd7c7e8770
CloverMidori <youhuid05@gmail.com>
2021-06-04 23:12:38 +0900

    あ

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(function (req, res, next) {
 
 app.use(subdomain('bot', botRouter));
 
-app.use(subdomain('api', testRouter));
+app.use(subdomain('test', testRouter));
 
 app.use(subdomain('api', apiRouter));
 
