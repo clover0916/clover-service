8c1c415a44900de5df8cdd696e156000b11bdce9
CloverMidori <youhuid05@gmail.com>
2021-09-12 11:18:21 +0900

    Test

--- routes/api.js
+++ routes/api.js
@@ -31,9 +31,11 @@ router.get('/video_info', async function(req, res, next) {
 });
 
 router.post('/img2webp', multer({ dest: './img2webp' }).array('files', 1), async function(req, res, next) {
+  console.log('--- req.body --')
+  console.log(req.body)
   console.log('--- req.files ---')
   console.log(req.files)
-  res.send(req.file.originalname + 'ファイルのアップロードが完了しました。');
+  res.send(req.files[0].originalname + 'ファイルのアップロードが完了しました。');
 });
 
 module.exports = router;
