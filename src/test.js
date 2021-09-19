00f943659ff8b155115a742587af2ae891bcc8e1
CloverMidori <youhuid05@gmail.com>
2021-09-19 11:20:00 +0900

    Test

--- src/test.js
+++ src/test.js
@@ -19,6 +19,22 @@ const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
   return new MDCRipple(el);
 });
 
+window.WebFontConfig = {
+  google: { families: ['Roboto', 'Noto+Sans+JP', 'Material+Icons'] },
+  typekit: { id: 'pbk7rhd' },
+  active: function() {
+    sessionStorage.fonts = true;
+  }
+};
+(function() {
+  var wf = document.createElement('script');
+  wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+  wf.type = 'text/javascript';
+  wf.async = 'true';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(wf, s);
+})();
+
 const listEl = document.querySelector('.mdc-drawer .mdc-deprecated-list');
 const mainContentEl = document.querySelector('.main-content');
 
