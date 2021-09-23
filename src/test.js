9fadfb52384f0bf1e82c660b2a310163c83c5c61
CloverMidori <youhuid05@gmail.com>
2021-09-23 10:07:52 +0900

    Test

--- src/test.js
+++ src/test.js
@@ -17,10 +17,9 @@ const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 const topAppBar = new MDCTopAppBar(topAppBarElement);
 const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
 const iconToggle = new MDCIconButtonToggle(document.querySelector('.demo-icon'));
-const listEls = MDCList.attachTo(document.querySelector('.mdc-deprecated-list'));
-listEls.wrapFocus = true;
+const list = MDCList.attachTo(document.querySelector('.mdc-deprecated-list'));
+list.wrapFocus = true;
 const listEl = document.querySelector('.mdc-drawer .mdc-deprecated-list');
-const dialog_list = new MDCList(document.querySelector('.mdc-dialog .mdc-deprecated-list'));
 //Ripple
 
 const mainContentEl = document.querySelector('.main-content');
