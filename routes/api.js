6223f9d543f3c475fb5435e9050672fbf609d59f
CloverMidori <youhuid05@gmail.com>
2021-11-11 11:21:31 +0900

    Test

--- routes/api.js
+++ routes/api.js
@@ -16,7 +16,7 @@ router.get('/get_video', async function(req, res, next) {
     const info = await ytdl.getInfo(URL);
     video.pipe(res);
   } catch (err) {
-    res.send("Too many request.")
+    res.send(JSON.stringify(err))
   }
 });
 
