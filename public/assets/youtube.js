ce2a353b29afaac925b9268129f7616e2363db73
CloverMidori <youhuid05@gmail.com>
2021-12-23 23:46:50 +0900

    Test

--- public/assets/youtube.js
+++ public/assets/youtube.js
@@ -6,6 +6,9 @@ fetch('https://api.clover-midori.net/video_info?id=' + id, { method: 'GET' })
   .then(info => {
     if(info.error) return alert('エラーが発生しました、一旦時間をおいてください');
     var mimeCodec = `${info.formats[0].mimeType}; codecs="${info.formats[0].codecs}"`
+    alert(mimeCodec)
+    document.getElementById('video_title').innerHTML = info.player_response.videoDetails.title
+    document.getElementById('video_description').innerHTML = info.player_response.videoDetails.shortDescription
 
     if ('MediaSource' in window && MediaSource.isTypeSupported(mimeCodec)) {
       var mediaSource = new MediaSource;
